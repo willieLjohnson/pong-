1d0cbe6c6ab6cdd72c61f3124e17c1197b59f227
Willie Johnson <liwa.johnson@gmail.com>
2021-12-19 19:53:49 -0500

    setup super basic project with just black screen

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <SDL2/SDL.h>
+
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 729;
+
+int main() {
+    // Init
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    // Logic
+    {
+        bool running = true;
+
+        while (running) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) {
+                    running = false;
+                } else if (event.type == SDL_KEYDOWN) {
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+                        running = false;
+                    }
+                }
+            }
+
+            SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
+            SDL_RenderClear(renderer);
+
+            SDL_RenderPresent(renderer);
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
