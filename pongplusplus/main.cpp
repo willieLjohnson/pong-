86b8bc6743768c7546ae413e242878d503b49ba1
Willie Johnson <liwa.johnson@gmail.com>
2021-12-19 19:01:29 -0500

    Initial Commit

---
+++ pongplusplus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pongplusplus
+//
+//  Created by Willie Liwa Johnson on 12/19/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
