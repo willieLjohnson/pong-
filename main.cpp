b2ef47d4884029cb574dc59f2ff1de1624fcb2b0
Willie Johnson <liwa.johnson@gmail.com>
2021-12-20 17:10:17 -0500

    Add Vec2 class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,34 @@
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 729;
 
+class Vec2 {
+    public:
+        Vec2()
+            : x(0.0f), y(0.0f)
+        {}
+
+        Vec2(float x, float y)
+            : x(x), y(y)
+        {}
+
+        Vec2 operator+(Vec2 const& rhs) {
+            return Vec2(x + rhs.x, y + rhs.y);
+        }
+
+        Vec2& operator+=(Vec2 const& rhs) {
+            x += rhs.x;
+            y += rhs.y;
+            
+            return *this;
+        }
+
+        Vec2 operator*(float rhs) {
+            return Vec2(x * rhs, y * rhs);
+        }
+
+        float x, y;
+};
+
 int main() {
     // Init
     SDL_Init(SDL_INIT_VIDEO);
