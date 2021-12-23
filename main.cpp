c6bd446e545f11000f9b645d41251247fe1559ef
Willie Johnson <liwa.johnson@gmail.com>
2021-12-23 17:33:00 -0500

    add game font

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 729;
 
 const int BALL_WIDTH = 15;
 const int BALL_HEIGHT = 15;
+
+const int PADDLE_WIDTH = 15;
+const int PADDLE_HEIGHT = 30;
 
 class Vec2 {
     public:
@@ -36,8 +40,7 @@ class Vec2 {
 
 class Ball {
     public:
-        Ball(Vec2 position)
-            : position(position) {
+        Ball(Vec2 position) : position(position) {
                 rect.x = static_cast<int>(position.x);
                 rect.y = static_cast<int>(position.y);
                 rect.w = BALL_WIDTH;
@@ -55,15 +58,43 @@ class Ball {
         SDL_Rect rect;
 };
 
+class Paddle {
+    public:
+        Paddle(Vec2 position) : position(position) {
+                rect.x = static_cast<int>(position.x);
+                rect.y = static_cast<int>(position.y);
+                rect.w = PADDLE_WIDTH;
+                rect.h = PADDLE_HEIGHT;
+            }
+
+        void Draw(SDL_Renderer* renderer) {
+            rect.x = static_cast<int>(position.x);
+            rect.y = static_cast<int>(position.y);
+
+            SDL_RenderFillRect(renderer, &rect);
+        }
+
+
+        Vec2 position;
+        SDL_Rect rect;
+};
+
 int main() {
     // Init
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
     SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
+    TTF_Font* scoreFont = TTF_OpenFont("DejaVuSansMono.ttf", 40);
+
     Ball ball(Vec2((WINDOW_WIDTH / 2.0f) - (BALL_WIDTH / 2.0f),
 	(WINDOW_HEIGHT / 2.0f) - (BALL_WIDTH / 2.0f)));
+
+    Paddle paddleOne(Vec2(50.0f, (WINDOW_HEIGHT / 2.0f) - (PADDLE_HEIGHT / 2.0f)));
+    Paddle paddleTwo(Vec2(WINDOW_WIDTH - 50.0f, (WINDOW_HEIGHT / 2.0f) - (PADDLE_HEIGHT / 2.0f)));
+
     // Logic
     {
         bool running = true;
@@ -93,12 +124,17 @@ int main() {
 
             ball.Draw(renderer);
 
+            paddleOne.Draw(renderer);
+            paddleTwo.Draw(renderer);
+
             SDL_RenderPresent(renderer);
         }
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_CloseFont(scoreFont);
+    TTF_Quit();
     SDL_Quit();
 
     return 0;
